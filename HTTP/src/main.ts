0e72611ce6a4b13f3fb29a3dc4038a09b4060a4c
Bryan Vásquez <159486619+BryanVas24@users.noreply.github.com>
2025-01-29 15:42:45 -0600

    Usando httpClient

--- HTTP/src/main.ts
+++ HTTP/src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-
-
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+import { provideHttpClient } from '@angular/common/http';
+//Manera de usar el httpClient en donde sea de la app
+bootstrapApplication(AppComponent, { providers: [provideHttpClient()] }).catch(
+  (err) => console.error(err)
+);
