f06d6b68ce61cd5c80f2b5851164c40839810625
Bryan Vásquez <159486619+BryanVas24@users.noreply.github.com>
2025-01-29 19:22:20 -0600

    Haciendo un get

--- HTTP/src/app/places/available-places/available-places.component.ts
+++ HTTP/src/app/places/available-places/available-places.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
@@ -12,8 +12,23 @@ import { HttpClient } from '@angular/common/http';
   styleUrl: './available-places.component.css',
   imports: [PlacesComponent, PlacesContainerComponent],
 })
-export class AvailablePlacesComponent {
+export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
   //Esto es para inyectar el HtttpClient
   private httpClient = inject(HttpClient);
+  private destroy = inject(DestroyRef);
+  //Manera de hacer un get (el onInit es practicamente el useEffect)
+  ngOnInit() {
+    const suscription = this.httpClient
+      .get<{ places: Place[] }>('http://localhost:3000/places')
+      .subscribe({
+        next: (resdata) => {
+          console.log(resdata);
+        },
+      });
+    //no es necesario pero es para limpiar la suscripción http
+    this.destroy.onDestroy(() => {
+      suscription.unsubscribe();
+    });
+  }
 }
