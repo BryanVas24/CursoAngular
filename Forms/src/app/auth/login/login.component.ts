d00c183521294b31a7470e2e433ba06dad1aae07
Bryan Vásquez <159486619+BryanVas24@users.noreply.github.com>
2025-02-17 12:02:43 -0600

    reiniciando todo a como estaba antes para usar el enfoque de formularios reactivos

--- Forms/src/app/auth/login/login.component.ts
+++ Forms/src/app/auth/login/login.component.ts
@@ -1,58 +1,9 @@
-import {
-  afterNextRender,
-  Component,
-  DestroyRef,
-  inject,
-  viewChild,
-} from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
-import { debounceTime } from 'rxjs';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {
-  private form = viewChild.required<NgForm>('form');
-  private destroyRef = inject(DestroyRef);
-  constructor() {
-    //Esto se ejecuta hasta que el formulario este construido
-    afterNextRender(() => {
-      const savedForm = window.localStorage.getItem('saved-login-form');
-      if (savedForm) {
-        const loadedFormData = JSON.parse(savedForm);
-        const savedEmail = loadedFormData.email;
-        setTimeout(() => {
-          this.form().controls['email'].setValue(savedEmail);
-        }, 1);
-      }
-      //Esto miralo como un handler change
-      const suscription = this.form()
-        //el debounceTime evita que la función next se ejecute a cada rato
-        .valueChanges?.pipe(debounceTime(500))
-        .subscribe({
-          next: (value) =>
-            window.localStorage.setItem(
-              'saved-login-form',
-              JSON.stringify({
-                email: value.email,
-              })
-            ),
-        });
-      this.destroyRef.onDestroy(() => suscription?.unsubscribe());
-    });
-  }
-  onSubmit(formData: NgForm) {
-    if (formData.form.invalid) {
-      return;
-    }
-    //Así se obtiene el contenido del form
-    const email = formData.form.value.email;
-    const password = formData.form.value.password;
-    //Esto resetea el formulario
-    formData.form.reset();
-  }
-}
+export class LoginComponent {}
