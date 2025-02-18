c2240e9a86a95d3c8e20d411d577b62f33f58ced
Bryan Vásquez <159486619+BryanVas24@users.noreply.github.com>
2025-02-18 13:15:41 -0600

    usando validators

--- Forms/src/app/auth/login/login.component.ts
+++ Forms/src/app/auth/login/login.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,12 +14,35 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  //Este get se usa porque la validación en el if de la plantilla era muy larga
+  get emailIsInvalidCondition() {
+    return (
+      this.form.controls.email.touched &&
+      this.form.controls.email.dirty &&
+      this.form.controls.email.invalid
+    );
+  }
+  get passwordInvalidCondition() {
+    return (
+      this.form.controls.password.touched &&
+      this.form.controls.password.dirty &&
+      this.form.controls.password.invalid
+    );
+  }
   //Es una instancia de un FormGroup
   form = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl(''),
+    //Para añadir validaciones podes usar un [] de validators o {} configuración
+    //Los validators son funciones built-in o construidas manualmente
+    email: new FormControl('', {
+      validators: [Validators.email, Validators.required],
+    }),
+    password: new FormControl('', {
+      validators: [Validators.minLength(6), Validators.required],
+    }),
   });
   onSubmit() {
+    //Esto es para hacer validaciones pero es avanzado
+    //this.form.controls.email.addValidators
     const enteredEmail = this.form.value.email;
     const enteredPassword = this.form.value.password;
     console.log(enteredEmail, enteredPassword);
