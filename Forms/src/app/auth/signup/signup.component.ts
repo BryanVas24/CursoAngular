65f043e319ff9e0b1ffe7bd2c66f6fc8f95eaa46
Bryan Vásquez <159486619+BryanVas24@users.noreply.github.com>
2025-02-19 13:02:18 -0600

    Primera parte de tomar password y email lista

--- Forms/src/app/auth/signup/signup.component.ts
+++ Forms/src/app/auth/signup/signup.component.ts
@@ -1,9 +1,35 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
+  //Super importante
+  imports: [ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
 })
-export class SignupComponent {}
+export class SignupComponent {
+  form = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.email, Validators.required],
+    }),
+    password: new FormControl('', {
+      validators: [Validators.minLength(6), Validators.required],
+    }),
+  });
+
+  onSubmit() {
+    const enteredEmail = this.form.value.email;
+    const enterePassword = this.form.value.password;
+    console.log(enterePassword, enteredEmail);
+  }
+  onReset() {
+    this.form.reset();
+  }
+}
