06da73d6a8af08b7a253efd84f79e9252d24851f
Bryan Vásquez <159486619+BryanVas24@users.noreply.github.com>
2025-02-24 12:47:24 -0600

    Usando routerLink

--- Routing/src/app/app.routes.ts
+++ Routing/src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { TasksComponent } from './tasks/tasks.component';
+import { NoTaskComponent } from './tasks/no-task/no-task.component';
 //Esto te permite definir un array de rutas
 export const routes: Routes = [
+  {
+    //esto es el equivalente a "/"
+    path: '',
+    component: NoTaskComponent,
+  },
   {
     path: 'tasks',
     component: TasksComponent,
