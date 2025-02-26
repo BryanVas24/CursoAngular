8b7b3ee091807887dff745f8dbbe9a76a3d70311
Bryan Vásquez <159486619+BryanVas24@users.noreply.github.com>
2025-02-26 12:24:01 -0600

    Usando nested routes

--- Routing/src/app/app.routes.ts
+++ Routing/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { TasksComponent } from './tasks/tasks.component';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
+import { NewTaskComponent } from './tasks/new-task/new-task.component';
 //Esto te permite definir un array de rutas
 export const routes: Routes = [
   {
@@ -12,5 +13,10 @@ export const routes: Routes = [
   {
     path: 'users/:userId',
     component: UserTasksComponent,
+    //la nested route
+    children: [
+      { path: 'tasks', component: TasksComponent },
+      { path: 'tasks/new', component: NewTaskComponent },
+    ],
   },
 ];
--- Routing/src/app/users/user-tasks/user-tasks.component.ts
+++ Routing/src/app/users/user-tasks/user-tasks.component.ts
@@ -1,16 +1,40 @@
-import { Component, computed, inject, input } from '@angular/core';
+import {
+  Component,
+  computed,
+  DestroyRef,
+  inject,
+  input,
+  OnInit,
+} from '@angular/core';
 import { UsersService } from '../users.service';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-user-tasks',
   standalone: true,
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
+  imports: [RouterOutlet],
 })
-export class UserTasksComponent {
+export class UserTasksComponent implements OnInit {
   //obteniendo parametros de la URL
+  userName = '';
   userId = input.required<string>();
   private userService = inject(UsersService);
-  userName = computed(
+  private activedRoute = inject(ActivatedRoute);
+  private destroyRef = inject(DestroyRef);
+  //forma nueva
+  /*userName = computed(
     () => this.userService.users.find((u) => u.id === this.userId())?.name
-  );
+  );*/
+
+  //forma vieja
+  ngOnInit(): void {
+    const suscription = this.activedRoute.paramMap.subscribe({
+      next: (paramMap) =>
+        (this.userName =
+          this.userService.users.find((u) => u.id === paramMap.get('userId'))
+            ?.name || ''),
+    });
+    this.destroyRef.onDestroy(() => suscription.unsubscribe());
+  }
 }
